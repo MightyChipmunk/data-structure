7ef7d284c4c78a05eac35e355b531a886543b61f
MightyChipmunk <21qqq4@gmail.com>
2022-02-22 15:41:49 +0900

    미로 생성, 플레이어 생성, 우수법 길찾기 적용

---
+++ Algorithm/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            Player player = new Player();   
+            board.initialize(25, player);
+            player.Initialize(1, 1, board);
+
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+
+            int lastTIck = 0;
+
+            while(true)
+            {
+                int currentTick = System.Environment.TickCount;
+                if (currentTick - lastTIck < WAIT_TICK)
+                    continue;
+                int deltaTick = currentTick - lastTIck;
+                lastTIck = currentTick;
+                //입력
+
+                //로직
+                player.Update(deltaTick);
+                //렌더링
+                Console.SetCursorPosition(0, 0);
+                board.Render();
+            }
+        }
+    }
+}
